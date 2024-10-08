4a522610ac00e66ed0851a51e69c524b47fcbcca
alexanderr4 <jose231101a@gmail.com>
2024-10-07 21:42:35 -0500

    Arreglo permiso usuarios

--- src/main/java/edu/uptc/PizonAcevedo/controller/UserController.java
+++ src/main/java/edu/uptc/PizonAcevedo/controller/UserController.java
@@ -10,6 +10,7 @@ import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Collection;
@@ -20,6 +21,7 @@ import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/users")
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class UserController {
 
 //    @Value("${message}")
@@ -67,8 +69,8 @@ public class UserController {
         }
     }
 
-    @PreAuthorize("hasRole('ADMIN')")
     @RequestMapping(value = "/getUsers", method = RequestMethod.GET, produces = "application/json")
+    @PreAuthorize("hasRole('ADMIN')")
     public ResponseEntity getUsers(){
         return new ResponseEntity<>(userMgmt.getUsers(), HttpStatus.OK);
     }
